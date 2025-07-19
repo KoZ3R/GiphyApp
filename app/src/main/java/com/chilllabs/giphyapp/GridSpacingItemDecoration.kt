099d48a5ac81238028388f4769ab1b0d1a6891e9
Vladislav <pudanvladislav@gmail.com>
2025-07-19 14:31:34 +0300

    Alpha-test

--- app/src/main/java/com/chilllabs/giphyapp/GridSpacingItemDecoration.kt
+++ app/src/main/java/com/chilllabs/giphyapp/GridSpacingItemDecoration.kt
@@ -2,41 +2,33 @@ package com.chilllabs.giphyapp
 
 import android.content.Context
 import android.graphics.Rect
+import android.util.TypedValue
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
 class GridSpacingItemDecoration(
     private val spanCount: Int,
-    private val spacingDp: Int,
-    private val includeEdge: Boolean = true
+    context: Context,
+    spacingDp: Float = 2f
 ) : RecyclerView.ItemDecoration() {
 
-    override fun getItemOffsets(
-        outRect: Rect,
-        view: View,
-        parent: RecyclerView,
-        state: RecyclerView.State
-    ) {
+    private val spacingPx: Int = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        spacingDp,
+        context.resources.displayMetrics
+    ).toInt()
+
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
         val position = parent.getChildAdapterPosition(view)
+        if (parent.getChildViewHolder(view) is GifAdapter.LoadingViewHolder) {
+            outRect.set(0, 0, 0, 0)
+            return
+        }
+
         val column = position % spanCount
-
-        if (includeEdge) {
-            outRect.left = spacingDp - column * spacingDp / spanCount
-            outRect.right = (column + 1) * spacingDp / spanCount
-            outRect.top = if (position < spanCount) spacingDp else 0
-            outRect.bottom = spacingDp
-        } else {
-            outRect.left = column * spacingDp / spanCount
-            outRect.right = spacingDp - (column + 1) * spacingDp / spanCount
-            outRect.top = if (position < spanCount) 0 else spacingDp
-            outRect.bottom = 0
-        }
-    }
-
-    companion object {
-        fun dpToPx(dp: Int, context: Context): Int {
-            val density = context.resources.displayMetrics.density
-            return (dp * density).toInt()
-        }
+        outRect.left = spacingPx - (column * spacingPx / spanCount)
+        outRect.right = ((column + 1) * spacingPx / spanCount)
+        outRect.top = if (position < spanCount) 0 else spacingPx
+        outRect.bottom = spacingPx
     }
 }
--- app/src/main/java/com/chilllabs/giphyapp/RetrofitClient.kt
+++ app/src/main/java/com/chilllabs/giphyapp/RetrofitClient.kt
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
     private const val BASE_URL = "https://api.giphy.com/v1/"
-    internal const val API_KEY = "iBYRKgezch8d6Vfkc2TXuyisnBrKGqkZ"
+    internal const val API_KEY = "c1oY3nmgtplq0oOz1KBVuW00DWREW3oV"
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
     }
